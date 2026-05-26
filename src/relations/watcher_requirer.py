c0a5761769b45f7c8377f399597c792636564065
Dragomir Penev <dragomir.penev@canonical.com>
2026-05-26 17:09:01 +0300

    Try to be less restrictive when cleaning up raft

--- src/relations/watcher_requirer.py
+++ src/relations/watcher_requirer.py
@@ -232,6 +232,9 @@ class WatcherRequirerHandler(Object):
             return name
         return f"relation-{relation.id}"
 
+    def _get_related_ips(self, relation: Relation) -> list[str]:
+        return [data["unit-address"] for data in relation.data.values() if "unit-address" in data]
+
     def _get_standby_clusters(self, relation: Relation) -> list[str]:
         """Get related standby clusters from the relation app data.
 
@@ -327,7 +330,7 @@ class WatcherRequirerHandler(Object):
                         new_address, raft_password, partner_addrs, port
                     )
                 raft_controller.cleanup_raft_cluster(
-                    new_address, raft_password, partner_addrs, port
+                    new_address, raft_password, self._get_related_ips(relation), port
                 )
 
     def _on_update_status(self, event: UpdateStatusEvent) -> None:
