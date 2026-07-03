c7fb8fb9c982fc7ac1a2b4229df6a4007faaf5b5
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2026-07-03 14:12:49 +0300

    [MISC] Update single kernel (#60)
    * Update single kernel
    
    * Add kernel to int tests

--- src/raft_controller.py
+++ src/raft_controller.py
@@ -36,6 +36,7 @@ from jinja2 import Template
 from pysyncobj.utility import TcpUtility
 from single_kernel_postgresql.config.enums import Substrates
 from single_kernel_postgresql.config.literals import PATRONI_CLUSTER_STATUS_ENDPOINT
+from single_kernel_postgresql.managers.patroni import ClusterMember
 from single_kernel_postgresql.utils import (
     create_directory,
     parallel_patroni_get_request,
@@ -43,7 +44,6 @@ from single_kernel_postgresql.utils import (
 )
 from tenacity import RetryError, Retrying, stop_after_attempt, wait_fixed
 
-from cluster import ClusterMember
 from constants import RAFT_PARTNER_PREFIX, RAFT_PORT
 
 if TYPE_CHECKING:
