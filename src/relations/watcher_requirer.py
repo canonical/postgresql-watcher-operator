faaa3672d79b43866944eadf142007bc6ea48a39
Xavier Pedrós <xavier.pedros@canonical.com>
2026-09-22 09:48:35 +0200

    No blocker in case IP address not set

--- src/relations/watcher_requirer.py
+++ src/relations/watcher_requirer.py
@@ -15,13 +15,13 @@ Multi-cluster support:
 - Each RaftController uses instance-specific data directories and systemd services
 """
 
+import errno
 import json
 import logging
 import os
+import socket
 import typing
 from datetime import datetime
-import errno
-import socket
 
 from charmlibs.systemd import service_running
 from ops import (
@@ -121,15 +121,26 @@ class WatcherRequirerHandler(Object):
         return "disable-watcher" in relation.data[relation.app]
 
     def port_in_use(self, port: int) -> bool:
-        """Return True if port is already in use by another process"""
+        """Return True if port is already in use by another process.
+
+        If IP address isn't valid, assume we are either testing or
+        unit has no address yet.
+        """
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             try:
                 sock.bind((self.unit_ip, port))
+            except socket.gaierror:
+                logger.warning("Cannot resolve %s, skipping probe for port %s", self.unit_ip, port)
+                return False
             except OSError as e:
                 if e.errno in (errno.EADDRINUSE, errno.EACCES):
                     return True
+                if e.errno == errno.EADDRNOTAVAIL:
+                    logger.warning("%s is not a local address, skipping probe", self.unit_ip)
+                    return False
                 raise
+
         return False
 
     def _get_port_for_relation(self, relation_id: int) -> int:
